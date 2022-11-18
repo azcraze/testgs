2330ce685e45aa7a46061532a5120e5598219fc4
azcraze <15200975+azcraze@users.noreply.github.com>
2022-11-18 11:07:43 -0600

    create my.js

---
+++ my.js
@@ -0,0 +1,37 @@
+
+function testGS(){
+
+    const url = "https://script.google.com/macros/s/AKfycbzDIh4uhz59Hg-OD7oD6tJZd1iBHT1YuI7D7rUXWazhTz513cVifgTZrurO-tfMf8bgSA/exec";
+
+
+    fetch(url)
+        .then(d => d.json())
+        .then(d => {
+            document.getElementById("app").textContent = d[0].status;
+        });
+
+}
+
+
+function addGS(){
+
+    const url = "https://script.google.com/macros/s/AKfycbzDIh4uhz59Hg-OD7oD6tJZd1iBHT1YuI7D7rUXWazhTz513cVifgTZrurO-tfMf8bgSA/exec";
+
+
+    fetch(url,{
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        redirect: 'follow', // manual, *follow, error
+        //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({"date":"2022-11-18T06:00:00.000Z","month#":11,"player":"fishie","red":0,"blue":0,"rebel":12,"bonusred":1.5}) // body data type must match "Content-Type" header
+      });
+
+}
+
+document.getElementById("btn2").addEventListener("click",addGS);
+
+document.getElementById("btn").addEventListener("click",testGS);
